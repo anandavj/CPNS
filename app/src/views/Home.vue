fbe64e14627b4378bc8e1bd205ef0ad655387e63
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-01-10 14:36:30 +0700

    add color theme option

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -18,7 +18,7 @@
             <div class="subheading mb-4 text-center"></div>
             <v-btn
               class="mt-12"
-              color="orange darken-2"
+              color="primary"
               dark
               large
               href="/pre-made-themes"
@@ -105,7 +105,7 @@
             <em>Kick-start your application today</em>
             <v-btn
               class="mt-12"
-              color="orange darken-2"
+              color="primary"
               dark
               large
               href="http://retail.buanapaksa.com"
@@ -173,7 +173,7 @@
         </v-container>
       </section>
 
-      <v-footer color="orange darken-2">
+      <v-footer color="primary">
         <v-layout row wrap align-center>
           <v-flex xs12>
             <div class="white--text ml-4">
