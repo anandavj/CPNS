c48ee620c15638170412c592de6a6a15332029c8
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-01-20 13:08:00 +0700

    modify karyawan

--- app-karyawan/src/App.vue
+++ app-karyawan/src/App.vue
@@ -28,7 +28,7 @@
               <v-list-item-title>Lihat Karyawan</v-list-item-title>
               <v-list-item-icon><v-icon>mdi-account-group</v-icon></v-list-item-icon>
             </v-list-item>
-            <v-list-item link class="caption">
+            <v-list-item link class="caption" @click="goTo('managementKaryawan','Pengaturan Karyawan')">
               <v-list-item-title>Pengaturan Karyawan</v-list-item-title>
               <v-list-item-icon><v-icon>mdi-account-key-outline</v-icon></v-list-item-icon>
             </v-list-item>
--- app-karyawan/src/views/ManagementKaryawan.vue
+++ app-karyawan/src/views/ManagementKaryawan.vue
@@ -1,5 +1,15 @@
 <template>
     <v-app>
-        
+        <v-container fluid>
+            <v-row>
+                <v-col cols="12">
+                    <v-row align="center" justify="center">
+                        <v-card>
+                            <v-card-title></v-card-title>
+                        </v-card>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
