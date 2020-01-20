f01c17191ab3b9f618077a535a6b306e0cb19b05
Satria Kemal <satria.kemal.08@gmail.com>
2020-01-20 15:47:09 +0700

    opname

---
+++ app-karyawan/src/views/StockOpname.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-btn color="primary" text dark>
+        <v-card>
+            
+        </v-card>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
