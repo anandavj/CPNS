57e064c3cc000176a2dfd43199cafcb7952e7f0f
Satria Kemal <satria.kemal.08@gmail.com>
2020-01-21 11:38:51 +0700

    try

---
+++ app-karyawan/src/views/Try.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      scrollable fullscreen 
+      persistent :overlay="false"
+      max-width="500px"
+      transition="dialog-transition"
+  >
+      boi
+  </v-dialog>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            dialog:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
