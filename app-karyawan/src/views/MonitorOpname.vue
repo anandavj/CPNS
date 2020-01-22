8306c748a01e287b006a81b5d78215dfa04ee657
Satria Kemal <satria.kemal.08@gmail.com>
2020-01-22 15:58:33 +0700

    updated UI monitor opname

--- app-karyawan/src/views/MonitorOpname.vue
+++ app-karyawan/src/views/MonitorOpname.vue
@@ -1,13 +1,54 @@
 <template>
-  
+  <v-layout row wrap>
+    <v-text-field
+      v-model="searchPricelist"
+      placeholder="Cari Barang"
+      :solo="true"
+      :clearable="true"
+      append-icon="mdi-magnify"
+      class="ma-0"
+    />
+    <v-flex xs6>
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="date"
+            label="Tanggal"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" no-title scrollable day-format>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+    </v-flex>
+
+    
+
+    <v-spacer></v-spacer>
+  </v-layout>
 </template>
+
 
 <script>
 export default {
-
-}
+  data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false
+  })
+};
 </script>
-
-<style>
-
-</style>
