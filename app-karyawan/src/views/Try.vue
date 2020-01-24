f4aeb6fd576cb88f28d80a03037714d6a27b7ec2
Satria Kemal <satria.kemal.08@gmail.com>
2020-01-24 09:47:59 +0700

    just updated try

--- app-karyawan/src/views/Try.vue
+++ app-karyawan/src/views/Try.vue
@@ -1,25 +1,67 @@
 <template>
-  <v-dialog
-      v-model="dialog"
-      scrollable fullscreen 
-      persistent :overlay="false"
-      max-width="500px"
-      transition="dialog-transition"
-  >
-      boi
-  </v-dialog>
+  <div id="app">
+  <v-app id="inspire">
+    <v-card>
+      <v-card-title>
+        Nutrition
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+  </v-app>
+</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            dialog:true
-        }
-    }
-}
+  data() {
+    return {
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        {
+          text: "Category",
+          value: "category",
+          align: " d-none d-lg-table-cell"
+        },
+        {
+          text: "Calories",
+          value: "calories",
+          align: " d-none d-lg-table-cell"
+        },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" }
+      ],
+      desserts: [
+          {
+              name: "chocolate",
+              category: "choco",
+              fat: 20,
+          }
+      ]
+
+      
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
