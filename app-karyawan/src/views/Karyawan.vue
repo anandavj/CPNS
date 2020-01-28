b1dd75d89af972d589e1fb0be8dfc42c0011add0
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-01-28 15:55:47 +0700

    finishing Karyawan, but not 100% completed

--- app-karyawan/src/views/Karyawan.vue
+++ app-karyawan/src/views/Karyawan.vue
@@ -1,37 +1,192 @@
 <template>
     <v-app>
         <div>
+            <!-- *************************************************************************************************************** -->
+            <!-- Search Karyawan in karyawans Array -->
+            <!-- *************************************************************************************************************** -->
             <v-text-field
                 placeholder="Cari Karyawan"
                 :solo='true'
                 :clearable='true'
                 append-icon="mdi-magnify"
                 class="font-regular font-weight-light"
+                v-model="searchKaryawan"
             />
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Add Karyawan -->
+            <!-- *************************************************************************************************************** -->
+            <v-container class="my-n7">
+                <v-dialog v-model="popUpNew" persistent max-width='1000px'>
+                    <template v-slot:activator="{ on }">
+                        <v-row justify="end">
+                            <v-col cols="5" lg="2">
+                                <v-btn block v-on="on" color="light-blue" class="white--text">
+                                    <v-icon>mdi-plus</v-icon>Tambah
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </template>
+                    <v-card>
+                        <v-toolbar dense flat>
+                            <span class="title font-weight-light">Tambah Karyawan</span>
+                            <v-btn absolute right icon @click="popUpNew = !popUpNew"><v-icon>mdi-close</v-icon></v-btn>
+                        </v-toolbar>
+                        <v-form ref="form">
+                            <v-card-text>
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-text-field label="ID" v-model="karyawan.id"/>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="Nama" v-model="karyawan.nama" :rules="nameRules"/>
+                                    </v-col>
+                                </v-row>
+                            </v-card-text>
+                        </v-form>
+                        <v-card-actions>
+                            <v-container>
+                                <v-row justify="center">
+                                    <v-btn class="mt-n5" color="red darken-1" text @click="close">Tidak</v-btn>
+                                    <v-btn class="mt-n5" color="blue darken-1" text @click="saveNewKaryawan">Ya</v-btn>
+                                </v-row>
+                            </v-container>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-container>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- List Data Karyawan -->
+            <!-- *************************************************************************************************************** -->
             <v-data-table
                 :headers="karyawanHeaders"
                 :items="karyawans"
+                :search="searchKaryawan"
+                @click:row="details"
                 :disable-sort="true"
-                :disable-filtering="true"
-                :mobile-breakpoint="1"
                 :hide-default-footer="true"
+                :mobile-breakpoint="1"
+                item-key="nama"
+                no-data-text="Data Karyawan Kosong"
+                no-results-text="Data Karyawan Tidak Ditemukan"
                 class="font-regular font-weight-light"
+                style="cursor:pointer"
             >
                 <template v-slot:item.actions="{ item }">
-                    <template>
-                        <v-btn @click="details(item)" color="light-blue" dark class="overline">Details</v-btn>
-                    </template>
+                    <v-icon class="mr-2" @click.stop="editKaryawan(item)">mdi-pencil</v-icon>
                 </template>
             </v-data-table>
-        <v-dialog v-model="popupDetails" fullscreen hide-overlay transition="dialog-bottom-transition">
-            <v-card>
-                <v-toolbar dense flat>
-                    <span class="title font-weight-light">Profil</span>
-                    <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
-                </v-toolbar>
-                {{karyawan}}
-            </v-card>
-        </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Details Karyawan -->
+            <!-- *************************************************************************************************************** -->
+            <!-- Phone / other xs sm device will display fullscreen dialog -->
+            <v-dialog v-if="popUpBreakPoint" v-model="popupDetails" fullscreen hide-overlay transition="dialog-bottom-transition">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    {{karyawan}}
+                </v-card>
+            </v-dialog >
+            <!-- Laptop/PC or other md lg device will not display fullscreen dialog -->
+            <v-dialog v-else v-model="popupDetails" width="1000px">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Detail Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    {{karyawan}}
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Edit Karyawan -->
+            <!-- *************************************************************************************************************** -->
+            <!-- Phone / other xs sm device will display fullscreen dialog -->
+            <v-dialog v-if="popUpBreakPoint" v-model="popUpEdit" fullscreen hide-overlay transition="dialog-bottom-transition">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Edit Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    <v-form ref="form">
+                        <v-card-text>
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field v-model="karyawan.nama" label="Nama" :rules="nameRules"/>
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                    </v-form>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                <v-btn class="mt-n12" color="blue darken-1" text @click="confirmSave">Save</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- Laptop/PC or other md lg device will not display fullscreen dialog -->
+            <v-dialog v-else persistent v-model="popUpEdit" width="1000px">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Edit Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    <v-form ref="form">
+                        <v-card-text>
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field v-model="karyawan.nama" label="Nama" :rules="nameRules"/>
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                    </v-form>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                <v-btn class="mt-n12" color="blue darken-1" text @click="confirmSave">Save</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Confirm on Edit -->
+            <!-- *************************************************************************************************************** -->
+            <v-dialog persistent v-model="popUpConfirmSave" width="500px">
+                <v-card>
+                    <v-card-title>Konfirmasi</v-card-title>
+                    <v-card-text>Apakah Anda Yakin ingin mengubah profil <b>{{tempNamaKaryawan}}</b>?</v-card-text>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n5" color="red darken-1" text @click="close">Tidak</v-btn>
+                                <v-btn class="mt-n5" color="blue darken-1" text @click="save">Ya</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
         </div>
     </v-app>
 </template>
@@ -43,20 +198,35 @@ export default {
     data() {
         return {
             karyawans: [
-                {no:1, nama:'Mahendra Fajar'},
-                {no:2, nama:'Ananda Vijaya'},
-                {no:3, nama:'Satria Kemal'},
+                {id:1, nama:'Mahendra Fajar'},
+                {id:2, nama:'Ananda Vijaya'},
+                {id:3, nama:'Satria Kemal'},
             ],
-            popupDetails: false,
             karyawan: {
-                no:null,
+                id:null,
                 nama:''
             },
             karyawanDefault: {
-                no:null,
+                id:null,
                 nama:''
             },
-            selectedIndex: -1
+            tempNamaKaryawan:'',
+            // This Var will be sent to the Database
+            editedKaryawan: [],
+            searchKaryawan:'',
+            popUpNew: false,
+            popupDetails: false,
+            popUpEdit: false,
+            popUpConfirmSave: false,
+            selectedIndex: -1,
+            // -----
+            // Rules
+            // -----
+            nameRules: [
+                v => !!v || 'Name Is Required'
+            ]
+            // -----
+            ,editedIndex: null       
         }
     },
     
@@ -67,21 +237,91 @@ export default {
             this.popupDetails = true
         },
         close() {
+            if(this.popupDetails) {
+                this.popupDetails = false
+                this.karyawan = Object.assign({},this.karyawanDefault)
+                this.selectedIndex = -1
+            } else {
+                if(this.popUpEdit) {
+                    this.popUpEdit = false
+                    this.karyawan = Object.assign({},this.karyawanDefault)
+                    this.selectedIndex = -1
+                } else {
+                    if(this.popUpConfirmSave) {
+                        this.popUpConfirmSave = false
+                        this.popUpEdit = true
+                    } else {
+                        if(this.popUpNew) {
+                            this.popUpNew = false
+                            this.karyawan = Object.assign({},this.karyawanDefault)
+                        }
+                    }
+                }
+            }
+        },
+        saveNewKaryawan() {
+            //API 
+            //Promise
+            if(this.$refs.form.validate()) {
+                this.karyawans.push(this.karyawan)
+                this.karyawan = Object.assign({},this.karyawanDefault)
+                this.close()
+            }
+        },
+        editKaryawan(item) {
+            this.selectedIndex = this.karyawans.indexOf(item)
+            this.tempNamaKaryawan = item.nama
+            this.karyawan = Object.assign({},item)
+            this.popUpEdit = true
+        },
+        // Save Karyawan
+        confirmSave() {
+            if(this.$refs.form.validate()) {
+                this.popUpEdit = false
+                this.popUpConfirmSave = true
+            }
+        },
+
+        //this need promise to ensure that the data in the db and vue in synced !!! IMPORTANT !!!
+        save() {
+            //to find the object inside editedKaryawan array
+            let obj = this.editedKaryawan.find( ({id}) => id === this.karyawan.id )
+            //to find the object inside karyawans
+            let obj2 = this.karyawans.find( ({id}) => id === this.karyawan.id )
+            if (obj) {
+                this.editedIndex = this.editedKaryawan.indexOf(obj)
+                this.editedKaryawan.splice(this.editedIndex,1)
+            }
+            //get all of the property name of karyawans
+            var loop = Object.getOwnPropertyNames(this.karyawans[this.karyawans.indexOf(obj2)])
+            //get rid of the __ob__ property to ensure that the looping is safe and secure
+            loop.splice(loop.indexOf('__ob__'),1)
+            //assign all the value of the property of obj2 in karyawans with karyawan
+            for(let i=0; i<loop.length; i++) {
+                this.karyawans[this.karyawans.indexOf(obj2)][loop[i]] = this.karyawan[loop[i]]
+            }
+            //save the edited karyawan into the array, the array will be sent into the db
+            this.editedKaryawan.push(this.karyawan)
+            this.popUpConfirmSave = false
             this.karyawan = Object.assign({},this.karyawanDefault)
-            this.selectedIndex = -1
-            this.popupDetails = false
         }
     },
 
     computed: {
         karyawanHeaders() {
             return [
-                {text:'No',value:'no',width:'20%',filter: () => true},
                 {text:'Nama',value:'nama'},
-                {text:'',value:'actions'}
+                {text:'',value:'actions',width:'5%'}
             ]
+        },
+        //view Breakpoint
+        popUpBreakPoint() {
+            if (this.$vuetify.breakpoint.name == 'xs' || this.$vuetify.breakpoint.name == 'sm') {
+                return true
+            } else {
+                return false
+            }
         }
     }
-    
 }
 </script>
