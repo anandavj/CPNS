d7990109fc7b8389f1436d92d7e49d9a297f3318
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-01-29 08:47:29 +0700

    adding tabs feature

--- app-karyawan/src/views/BarangMasukKeluar.vue
+++ app-karyawan/src/views/BarangMasukKeluar.vue
@@ -1,79 +1,163 @@
 <template>
     <v-app>
-        <h1 class="text-center font-weight-thin">Stock Update List</h1>
-        <v-container  class="mt-n3">
-            <v-dialog v-model="popUpModal" persistent max-width='600px'>
-                <template v-slot:activator="{ on }">
-                    <v-row justify="center" v-if="items.length == 0">
-                        <v-col lg="11">
-                            <v-card outlined class="text-center" v-on="on" color="light-green">
-                                <v-card-text class="white--text"><v-icon class="white--text">mdi-plus</v-icon>Tambah List</v-card-text>
-                            </v-card>
-                        </v-col>
-                    </v-row>
-                    <v-row justify="end" v-else>
-                        <v-col cols="5" lg="2">
-                            <v-btn block v-on="on" color="light-blue" class="white--text">
-                                <v-icon>mdi-plus</v-icon>Tambah
-                            </v-btn>
-                        </v-col>
-                    </v-row>
-                </template>
-                <v-card>
-                    <v-card-title>Tambah List</v-card-title>
-                    <v-card-text>
-                            <v-row>
-                                <v-col cols="12">
-                                    <v-text-field v-model="editableItem.nama" label="Nama Barang" required/>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-radio-group v-model="editableItem.status" row label="Status:">
-                                        <v-radio label="Masuk" value="masuk"/>
-                                        <v-radio label="Keluar" value="keluar"/>
-                                    </v-radio-group>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field v-model="editableItem.jumlah" label="Jumlah" required/>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-textarea v-model="editableItem.keterangan" outlined label="Keterangan"></v-textarea>
-                                </v-col>
-                            </v-row>
-                    </v-card-text>
-                    <v-card-actions>
+        <div class="mx-n4 my-n4 mx-md-n10 my-md-n11">
+            <v-card tile elevation="0">
+                <v-tabs
+                v-model="tab"
+                icons-and-text
+                grow
+                >
+                    <v-tabs-slider></v-tabs-slider>
+                    <v-tab href="#DOMasuk">
+                        <span class="caption">Delivery Order Masuk</span>
+                        <v-icon>mdi-arrow-down-bold</v-icon>
+                    </v-tab>
+                    <v-tab href="#DOKeluar">
+                        <span class="caption">Delivery Order Keluar</span>
+                        <v-icon>mdi-arrow-up-bold</v-icon>
+                    </v-tab>
+                </v-tabs>
+                <v-tabs-items v-model="tab">
+                    <!-- *************************************************************************************************************** -->
+                    <!-- Barang Masuk -->
+                    <!-- *************************************************************************************************************** -->
+                    <v-tab-item value="DOMasuk">
+                        <!-- Add Button -->
                         <v-container>
-                            <v-row justify="center">
-                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
-                                <v-btn class="mt-n12" color="blue darken-1" text @click="save">Save</v-btn>
-                            </v-row>
+                            <v-dialog v-model="popUpNewIn" persistent max-width='600px'>
+                                <template v-slot:activator="{ on }">
+                                    <!-- Add Button Ketika Belom ada list -->
+                                    <v-row justify="center" v-if="barangs.length == 0">
+                                        <v-col lg="11">
+                                            <v-card outlined v-on="on" color="light-green" class="text-center">
+                                                <v-card-text class="white--text"><v-icon class="white--text">mdi-plus</v-icon>Tambah</v-card-text>
+                                            </v-card>
+                                        </v-col>
+                                    </v-row>
+                                    <!-- Add Button Ketika Sudah ada list -->
+                                    <v-row justify="end" v-else>
+                                        <v-col cols="5" lg="2">
+                                            <v-btn block v-on="on" color="light-blue" class="white--text">
+                                                <v-icon>mdi-plus</v-icon>Tambah
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </template>
+                                <!-- Dialog Screen -->
+                                <v-card>
+                                    <v-toolbar dense flat>
+                                        <span class="title font-weight-light">Tambah Barang</span>
+                                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                                    </v-toolbar>
+                                    <v-form ref="form">
+                                        <v-card-text>
+                                            <v-row>
+                                                <v-col cols="12">
+                                                    <v-text-field v-model="barang.nama" label="Nama Barang"/>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-text-field v-model="barang.status" label="Status" disabled/>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-text-field v-model="barang.jumlah" label="Jumlah"/>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-textarea v-model="barang.keterangan" outlined label="Keterangan"/>
+                                                </v-col>
+                                            </v-row>
+                                        </v-card-text>
+                                    </v-form>
+                                    <v-card-actions>
+                                        <v-container>
+                                            <v-row justify="center">
+                                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                                <v-btn class="mt-n12" color="blue darken-1" text @click="saveNewBarang">Save</v-btn>
+                                            </v-row>
+                                        </v-container>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                            <v-data-table v-if="barangs.length > 0"
+                                :headers="headers"
+                                :items="barangs"
+                            >
+                            </v-data-table>
                         </v-container>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
-        </v-container>
-        <v-container v-if="items.length > 0" class="mt-n5">
-            <v-row justify="center">
-                <v-col lg="12">
-                    <v-data-table :headers="headers" :items="items" :disable-sort="true" :disable-filtering="true" :hide-default-footer="true">
-                        <template v-slot:item.actions="{ item }">
-                            <v-icon class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-                            <v-icon @click="confirmDelete(item)">mdi-delete</v-icon>
-                        </template>
-                    </v-data-table>
-                </v-col>
-            </v-row>
-        </v-container>
-        <v-container v-if="items.length > 0">
-            <v-row justify="end">
-                <v-col md="4" lg="2">
-                    <v-btn block outlined color="red">Cancel</v-btn>
-                </v-col>
-                <v-col md="4" lg="2">
-                    <v-btn block color="light-green" dark>Save</v-btn>
-                </v-col>
-            </v-row>
-        </v-container>
-        <v-dialog v-model="confirmModal" persistent max-width='350px'>
+                    </v-tab-item>
+                    <!-- *************************************************************************************************************** -->
+                    <!-- *************************************************************************************************************** -->
+
+                    <!-- *************************************************************************************************************** -->
+                    <!-- Barang Keluar -->
+                    <!-- *************************************************************************************************************** -->
+                    <v-tab-item value="DOKeluar">
+                        <v-container>
+                            <v-dialog v-model="popUpNewOut" persistent max-width='600px'>
+                                <template v-slot:activator="{ on }">
+                                    <!-- Add Button Ketika Belom ada list -->
+                                    <v-row justify="center" v-if="barangs.length == 0">
+                                        <v-col lg="11">
+                                            <v-card outlined v-on="on" color="light-green" class="text-center">
+                                                <v-card-text class="white--text"><v-icon class="white--text">mdi-plus</v-icon>Tambah</v-card-text>
+                                            </v-card>
+                                        </v-col>
+                                    </v-row>
+                                    <!-- Add Button Ketika Sudah ada list -->
+                                    <v-row justify="end" v-else>
+                                        <v-col cols="5" lg="2">
+                                            <v-btn block v-on="on" color="light-blue" class="white--text">
+                                                <v-icon>mdi-plus</v-icon>Tambah
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </template>
+                                <!-- Dialog Screen -->
+                                <v-card>
+                                    <v-card-title>Tambah List</v-card-title>
+                                    <v-card-text>
+                                        <v-row>
+                                            <v-col cols="12">
+                                                <v-text-field v-model="barang.nama" label="Nama Barang"/>
+                                            </v-col>
+                                            <v-col cols="12">
+                                                <v-text-field v-model="barang.status" label="Status" disabled>Masuk</v-text-field>
+                                            </v-col>
+                                            <v-col cols="12">
+                                                <v-text-field v-model="barang.jumlah" label="Jumlah"/>
+                                            </v-col>
+                                            <v-col cols="12">
+                                                <v-textarea v-model="barang.keterangan" outlined label="Keterangan"/>
+                                            </v-col>
+                                        </v-row>
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-row justify="center">
+                                            <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                            <v-btn class="mt-n12" color="blue darken-1" text @click="save">Save</v-btn>
+                                        </v-row>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-container>
+                    </v-tab-item>
+                    <!-- *************************************************************************************************************** -->
+                    <!-- *************************************************************************************************************** -->
+
+                </v-tabs-items>
+            </v-card>
+        </div>
+            <v-container v-if="barangs.length > 0">
+                <v-row justify="end">
+                    <v-col md="4" lg="2">
+                        <v-btn block outlined color="red">Cancel</v-btn>
+                    </v-col>
+                    <v-col md="4" lg="2">
+                        <v-btn block color="light-green" dark>Save</v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        
+        <v-dialog v-model="popUpConfirmSave" persistent max-width='350px'>
             <v-card>
                 <v-card-title>Confirmation</v-card-title>
                 <v-card-text>Apakah Anda Yakin Ingin Menghapus List Ini?</v-card-text>
@@ -101,60 +185,58 @@ export default {
                 {text:'Jumlah', value:'jumlah'},
                 {text:'',       value:'actions', width: '10%'}
             ],
-            items: [],
-            editableItem: {
+            barangs: [],
+            barang: {
                 nama:'',
                 status:'',
                 jumlah:'',
                 keterangan:''
             },
-            defaultItem: {
+            barangDefault: {
                 nama:'',
                 status:'',
                 jumlah:'',
                 keterangan:''
             },
-            popUpModal: false,
-            confirmModal: false,
-            editedIndex: -1
+            tab: null,
+            popUpNewIn: false,
+            popUpNewOut: false,
+            popUpConfirmSave: false,
+            selectedIndex: -1
         }
     },
     methods: {
-        save() {
-            if(this.editedIndex > -1) {
-                Object.assign(this.items[this.editedIndex],this.editableItem)
-            } else {
-                this.items.push(this.editableItem)
+        saveNewBarang() {
+            if(this.$refs.form.validate()) {
+                this.barangs.push(this.barang)
+                this.barang = Object.assign({},this.barangDefault)
+                this.close()
             }
-            this.close()
         },
         close() {
-            this.popUpModal = false
-            setTimeout(() => {
-                this.editableItem = Object.assign({}, this.defaultItem)
-                this.editedIndex = -1
-            }, 300)
+            if(this.popUpNewIn) {
+                this.popUpNewIn = false
+                this.barang = Object.assign({},this.barangDefault)
+            }
         },
         cancel() {
-            this.confirmModal = false
-            setTimeout(() => {
-                this.editedIndex = -1
-            }, 300)
+            
         },
-        editItem(item) {
-            this.editedIndex = this.items.indexOf(item)
-            this.editableItem = Object.assign({}, item)
-            this.popUpModal = true
+        editItem(/*item*/) {
+            
         },
-        confirmDelete(item) {
-            this.confirmModal = true
-            this.editedIndex = this.items.indexOf(item)
+        confirmDelete(/*item*/) {
+            
         },
         deleteItem() {
-            this.items.splice(this.editedIndex, 1)
-            this.editedIndex = -1
-            this.confirmModal = false
+            
         }
     }
 }
 </script>
+
+<style>
+    .v-slide-group__prev {
+        display: none !important;
+    }
+</style>>
