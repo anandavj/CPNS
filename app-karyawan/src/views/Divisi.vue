e01f33647077b7076a2944fce1080784ded32d59
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-02-01 11:30:48 +0700

    finishing divisi page

--- app-karyawan/src/views/Divisi.vue
+++ app-karyawan/src/views/Divisi.vue
@@ -1,11 +1,321 @@
 <template>
     <v-app>
+        <div>
+            <!-- *************************************************************************************************************** -->
+            <!-- Search Divisi in Divisis Array -->
+            <!-- *************************************************************************************************************** -->
+            <v-text-field
+                placeholder="Cari Divisi"
+                :solo="true"
+                :clearable="true"
+                append-icon="mdi-magnify"
+                class="font-regular font-weight-light"
+                v-model="searchDivisi"
+            />    
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Add Divisi -->
+            <!-- *************************************************************************************************************** -->
+            <v-btn fab dark large color="primary" fixed right bottom @click="popUpNew = !popUpNew">
+                <v-icon dark>mdi-plus</v-icon>
+            </v-btn>
+            <v-container class="my-n7">
+                <v-dialog v-model="popUpNew" persistent max-width='1000px'>
+                    <v-card>
+                        <v-toolbar dense flat>
+                            <span class="title font-weight-light">Tambah Divisi</span>
+                            <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                        </v-toolbar>
+                        <v-form ref="form">
+                            <v-card-text>
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-text-field label="Nama Divisi" v-model="divisi.nama"/>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <div class="title mt-n3">Permission</div>
+                                        <div v-for="(permission,index) in permissions" :key="index">
+                                            <v-checkbox
+                                                v-model="divisi.permissions"
+                                                :label="permission"
+                                                :value="permission"
+                                                class="font-weight-light mb-n2"
+                                            />
+                                        </div>
+                                    </v-col>
+                                </v-row>
+                            </v-card-text>
+                        </v-form>
+                        <v-card-actions>
+                            <v-container>
+                                <v-row justify="center">
+                                    <v-btn class="mt-n5" color="red darken-1" text @click="close">Tidak</v-btn>
+                                    <v-btn class="mt-n5" color="blue darken-1" text @click="saveNewDivisi">Ya</v-btn>
+                                </v-row>
+                            </v-container>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-container>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
         
+            <!-- *************************************************************************************************************** -->
+            <!-- List Divisi -->
+            <!-- *************************************************************************************************************** -->
+            <v-data-table
+                :headers="divisiHeaders"
+                :items="divisis"
+                @click:row="details"
+                :mobile-breakpoint="1"
+                :disable-sort="true"
+                :hide-default-footer="true"
+                no-data-text="Belum ada Divisi yang terdaftar"
+                no-results-text="Divisi tidak ditemukan"
+                class="font-regular font-weight-light"
+                style="cursor:pointer"
+            >
+                <template v-slot:item.actions="{ item }">
+                    <v-icon class="mr-2" @click.stop="editDivisi(item)">mdi-pencil</v-icon>
+                </template>
+            </v-data-table>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Details Divisi -->
+            <!-- *************************************************************************************************************** -->
+            <!-- Phone / other xs sm device will display fullscreen dialog -->
+            <v-dialog v-if="popUpBreakPoint" v-model="popUpDetails" fullscreen hide-overlay transition="dialog-bottom-transition">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Detail Divisi</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    {{divisi}}
+                </v-card>
+            </v-dialog>
+            <!-- Laptop/PC or other md lg device will not display fullscreen dialog -->
+            <v-dialog v-else v-model="popUpDetails" width="1000px">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Detail Divisi</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    {{divisi}}
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Edit Divisi -->
+            <!-- *************************************************************************************************************** -->
+            <v-dialog v-if="popUpBreakPoint" v-model="popUpEdit" fullscreen hide-overlay transition="dialog-bottom-transition">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Edit Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    <v-form ref="form">
+                        <v-card-text>
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field label="Nama Divisi" v-model="divisi.nama"/>
+                                </v-col>
+                                <v-col cols="12">
+                                    <div class="title mt-n3">Permission</div>
+                                    <div v-for="(permission,index) in permissions" :key="index">
+                                        <v-checkbox
+                                            v-model="divisi.permissions"
+                                            :label="permission"
+                                            :value="permission"
+                                            class="font-weight-light mb-n2"
+                                        />
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                    </v-form>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                <v-btn class="mt-n12" color="blue darken-1" text @click="confirmSave">Save</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- Laptop/PC or other md lg device will not display fullscreen dialog -->
+            <v-dialog v-else persistent v-model="popUpEdit" width="1000px">
+                <v-card>
+                    <v-toolbar dense flat>
+                        <span class="title font-weight-light">Edit Profil</span>
+                        <v-btn absolute right icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+                    </v-toolbar>
+                    <v-form ref="form">
+                        <v-card-text>
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field label="Nama Divisi" v-model="divisi.nama"/>
+                                </v-col>
+                                <v-col cols="12">
+                                    <div class="title mt-n3">Permission</div>
+                                    <div v-for="(permission,index) in permissions" :key="index">
+                                        <v-checkbox
+                                            v-model="divisi.permissions"
+                                            :label="permission"
+                                            :value="permission"
+                                            class="font-weight-light mb-n2"
+                                        />
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                    </v-form>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n12" color="red darken-1" text @click="close">Close</v-btn>
+                                <v-btn class="mt-n12" color="blue darken-1" text @click="confirmSave">Save</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+
+            <!-- *************************************************************************************************************** -->
+            <!-- Confirm on Edit -->
+            <!-- *************************************************************************************************************** -->
+            <v-dialog persistent v-model="popUpConfirmSave" width="500px">
+                <v-card>
+                    <v-card-title>Konfirmasi</v-card-title>
+                    <v-card-text>Apakah Anda Yakin ingin mengubah profil <b>{{divisi.nama}}</b>?</v-card-text>
+                    <v-card-actions>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-btn class="mt-n5" color="red darken-1" text @click="close">Tidak</v-btn>
+                                <v-btn class="mt-n5" color="blue darken-1" text @click="save">Ya</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <!-- *************************************************************************************************************** -->
+            <!-- *************************************************************************************************************** -->
+        </div>
     </v-app>
 </template>
 
 <script>
 export default {
-    
+    name: 'Divisi',
+
+    data() {
+        return {
+            divisiHeaders: [
+                {text:'Nama Divisi', value:'nama'},
+                {text:'', value:'actions'}
+            ],
+            divisis: [
+                {id:1, nama:'Manager', permissions: [
+                    'Add Barang'
+                ]}
+            ],
+            permissions: [
+                'Add Barang',
+                'View Barang',
+                'Update Barang'
+            ],
+            divisi: {
+                id:null,
+                nama:'',
+                permissions: []
+            },
+            divisiDefault: {
+                id:null,
+                nama:'',
+                permissions: []
+            },
+            searchDivisi:'',
+            popUpDetails: false,
+            popUpNew: false,
+            popUpEdit: false,
+            popUpConfirmSave: false,
+            selectedIndex: -1
+        }
+    },
+
+    methods: {
+        details(item) {
+            this.selectedIndex = this.divisis.indexOf(item)
+            this.divisi = Object.assign({},item)
+            this.popUpDetails = true
+        },
+        saveNewDivisi() {
+            this.divisis.push(this.divisi)
+            this.close()
+        },
+        editDivisi(item) {
+            this.selectedIndex = this.divisis.indexOf(item)
+            this.divisi = Object.assign({},item)
+            this.popUpEdit = true
+        },
+        confirmSave() {
+            if(this.$refs.form.validate()) {
+                this.popUpEdit = false
+                this.popUpConfirmSave = true
+            }
+        },
+        //this need promise to ensure that the data in the db and vue in synced !!! IMPORTANT !!!
+        save() {
+            //to find the object inside karyawans
+            let obj = this.divisis.find( ({id}) => id === this.divisi.id )
+            //assign all the value of the property of obj2 in karyawans with karyawan
+            this.divisis[this.divisis.indexOf(obj)].nama = this.divisi.nama
+            this.divisis[this.divisis.indexOf(obj)].permissions = this.divisi.permissions
+            this.popUpConfirmSave = false
+            this.divisi = Object.assign({},this.divisiDefault)
+        },
+        close() {
+            if(this.popUpDetails) {
+                this.divisi = Object.assign({},this.divisiDefault)
+                this.selectedIndex = -1
+                this.popUpDetails = false
+            } else {
+                if(this.popUpNew) {
+                    this.divisi = Object.assign({},this.divisiDefault)
+                    this.popUpNew = false
+                } else {
+                    if (this.popUpEdit) {
+                        this.popUpEdit = false
+                        this.divisi = Object.assign({},this.divisiDefault)
+                        this.selectedIndex = -1
+                    } else {
+                        if(this.popUpConfirmSave) {
+                            this.popUpConfirmSave = false
+                            this.popUpEdit = true
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    computed: {
+        //view Breakpoint
+        popUpBreakPoint() {
+            if (this.$vuetify.breakpoint.name == 'xs' || this.$vuetify.breakpoint.name == 'sm') {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
 }
 </script>
