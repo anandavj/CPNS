6fbb303f3e87453c6abf203bcb992aa31d40e371
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-04-13 07:38:36 +0700

    change endpoint

--- app-karyawan/src/main.js
+++ app-karyawan/src/main.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 import api from '@/api.js'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://localhost:8000/backend/api/'
+axios.defaults.baseURL = 'http://cpns.palugada.biz.id/backend/api'
 
 const config = {
   apiKey: "AIzaSyAGXPPSZkTQC0Qq5zoFmoDe3AdvbPBHYKE",
